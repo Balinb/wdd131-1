e6e221a709ed94eb8c7006579a983e3cf43a514d
Balinb <balinbenissan@gmail.com>
2024-06-01 00:16:29 +0200

    siteplan

--- week_2/temples.js
+++ week_2/temples.js
@@ -1,2 +1,10 @@
 var lastModified = document.lastModified;
 document.getElementById("modified").innerHTML = lastModified;
+// Hamburger response menu
+const hamButton = document.querySelector('#menu');
+const heading = document.querySelector('.heading');
+hamButton.addEventListener('click', () => {
+    heading.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
